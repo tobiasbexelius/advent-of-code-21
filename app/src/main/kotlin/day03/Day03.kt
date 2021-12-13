3f9783e78e7507eeddde869cf24d34ec6e7e3078
Tobias Bexelius <tobias.bexelius@hedvig.com>
2021-12-13 17:50:36 +0100

    Day 3

--- app/src/main/kotlin/day03/Day03.kt
+++ app/src/main/kotlin/day03/Day03.kt
@@ -11,11 +11,46 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay03Part1, ::solveDay03Part2)
 }
 
-
 fun solveDay03Part1(input: List<String>): Int {
-    TODO()
+    val wordSize = input.first().length
+    return input.asSequence()
+        .fold(IntArray(wordSize) { 0 }) { acc, s ->
+            s.forEachIndexed { index, c ->
+                if (c == '1') {
+                    acc[index]++
+                }
+            }
+            acc
+        }
+        .let { bitCount ->
+            val gamma = bitCount
+                .map { if (it >= input.size / 2) 1 else 0 }
+                .joinToString(separator = "")
+                .let { Integer.parseInt(it, 2) }
+            val ypsilon = bitCount
+                .map { if (it >= input.size / 2) 0 else 1 }
+                .joinToString(separator = "")
+                .let { Integer.parseInt(it, 2) }
+            gamma * ypsilon
+        }
 }
 
 fun solveDay03Part2(input: List<String>): Int {
-    TODO()
+    val wordSize = input.first().length
+    var ogrItems = input
+    var csrItems = input.toList()
+    repeat(wordSize) { index ->
+        ogrItems = ogrItems.partition { it[index] == '0' }
+            .let { if (it.first.size > it.second.size) it.first else it.second }
+        csrItems = csrItems.partition { it[index] == '0' }
+            .let {
+                if (
+                    it.second.isEmpty() ||
+                    (it.first.size <= it.second.size && it.first.isNotEmpty())
+                ) it.first else it.second
+            }
+    }
+    val ogr = ogrItems.single().let { Integer.parseInt(it, 2) }
+    val csr = csrItems.single().let { Integer.parseInt(it, 2) }
+    return ogr * csr
 }
